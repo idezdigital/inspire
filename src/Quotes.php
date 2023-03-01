cb5a8b3a75a0ea3ca60a0cae18da416367ab9efc
Pedro Santiago <pedro@idez.com.br>
2023-03-01 18:23:15 -0300

    first commit

---
+++ src/Quotes.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Idez\Inspire;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+class Quotes
+{
+    private const INSPIRING = [
+        'Se você pode sonhar, você pode fazer. - Walt Disney',
+    ];
+
+    /**
+     * Get an inspiring quote.
+     *
+     * Taylor & Dayle made this commit from Jungfraujoch. (11,333 ft.)
+     *
+     * May McGinnis always control the board. #LaraconUS2015
+     *
+     * RIP Charlie - Feb 6, 2018
+     *
+     * @return string
+     */
+    public static function inspire()
+    {
+        return self::format(Arr::random(self::INSPIRING));
+    }
+
+    /**
+     * Formats the given quote for a pretty console output.
+     *
+     * @param  string  $quote
+     * @return string
+     */
+    protected static function format($quote): string
+    {
+        [$text, $author] = str($quote)->explode('-');
+
+        return sprintf(
+            "\n  <options=bold>“ %s ”</>\n  <fg=gray>— %s</>\n",
+            trim($text),
+            trim($author),
+        );
+    }
+}
